f255228f7a010778a1e086f5791e2461d0c72238
Xinyi <117285647+ChengXinyi0@users.noreply.github.com>
2023-09-27 10:57:48 +0200

    Add files via upload

---
+++ 01_fastqc.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=2G
+#SBATCH --time=10:00:00
+#SBATCH --job-name=fastqc
+#SBATCH --mail-user=xinyi.cheng@students.unibe.ch
+#SBATCH --mail-type=begin,end
+#SBATCH --output=/data/users/xcheng/output_fastqc_%j.o
+#SBATCH --error=/data/users/xcheng/error_fastqc_%j.e
+#SBATCH --partition=pall
+#SBATCH --array=0-6
+
+
+#import fastqc
+
+module load UHTS/Quality_control/fastqc/0.11.9
+
+HOME=/data/users/xcheng/assembly_annotation_course
+RESOURCE=$HOME/participant_3
+
+
+
+for folder in $(ls $RESOURCE); do
+    for file in $(ls $RESOURCE/$folder); do
+        fastqc -o $HOME/read_QC/fastqc $RESOURCE/$folder/$file
+    done
+done
---
+++ 03_kerms_hist.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=40G
+#SBATCH --time=10:00:00
+#SBATCH --job-name=kmers_hist
+#SBATCH --mail-user=xinyi.cheng@students.unibe.ch
+#SBATCH --mail-type=begin,end
+#SBATCH --output=/data/users/xcheng/output_kmers_hist_%j.o
+#SBATCH --error=/data/users/xcheng/error_kmers_hist_%j.e
+#SBATCH --partition=pall
+
+
+# import jellyfish
+module load UHTS/Analysis/jellyfish/2.3.0
+
+
+
+INPUT=/data/users/xcheng/assembly_annotation_course/read_QC/kmer-counting
+
+for file in $(ls $INPUT); do
+    filename=$(echo "$file" | cut -d '.' -f 1)
+    echo $INPUT/$file
+    echo $INPUT/$filename
+    jellyfish histo -t 4 $INPUT/$file -o $INPUT/$filename.histo
+done
